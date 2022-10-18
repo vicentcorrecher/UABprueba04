7d0c1a7853d3419130da23ba521c3a49dba980bd
vimuozco <1661430@uab.cat>
2022-10-18 16:25:09 +0200

    Inicio + creación componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"" ,component:HeroesComponent},
+  {path:"Heroes" ,component:HomeComponent},
+  {path:"Home" ,component:HomeComponent},
+  {path:"About" ,component:AboutComponent},
+  {path:"**", component:HeroesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './comunes/header/header.component';
+import { FooterComponent } from './comunes/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
